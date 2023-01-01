3886ef1a7a6ec7c85369b35a91563403e3ed030b
Ozer <cemreozer.e@gmail.com>
2023-01-01 23:17:13 +0300

    env change

--- src/env.const.ts
+++ src/env.const.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  mongoURL: 'mongodb://localhost:27017/test',
+  mongoURL:
+    'mongodb+srv://doadmin:4X3K2vB9u7051fIW@db-mongodb-fra1-89437-9e716d71.mongo.ondigitalocean.com/admin?authSource=admin&tls=true',
   secret: 'secretKey',
 };
